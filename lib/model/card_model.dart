91cf4b4e2ecbd3431eb721bd75346360fe987bb1
Sakana398 <zephyrus398@gmail.com>
2025-01-14 15:49:48 +0800

    cardcolourchange

--- lib/model/card_model.dart
+++ lib/model/card_model.dart
@@ -11,8 +11,8 @@ class CardModel {
 }
 
 List<CardModel> cards = [
-  CardModel("Status", 0xFFec407a, Icons.auto_graph),
-  CardModel("Mental Training", 0xFF5c6bc0, Icons.psychology),
+  CardModel("Status", 0xFF5c6bc0, Icons.auto_graph),
+  CardModel("Mental Training", 0xFFec407a, Icons.psychology),
   CardModel("Community", 0xFFfbc02d, TablerIcons.friends),
   CardModel("Journal", 0xFF1565C0, Icons.book),
   CardModel("Songs", 0xFF2E7D32, Icons.music_note),
