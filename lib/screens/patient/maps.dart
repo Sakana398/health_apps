b0a4d57828fe342d370b3b25772fc7e687c0acd0
heyitssyakir <syakirnajmi83@gmail.com>
2025-01-21 03:23:16 +0800

    maps 2.0

--- lib/screens/patient/maps.dart
+++ lib/screens/patient/maps.dart
@@ -1,6 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:health_apps/model/locations.dart' as locations;
+import 'package:flutter/services.dart' show rootBundle;
 
 class MapsPage extends StatefulWidget {
   const MapsPage({super.key});
@@ -14,23 +15,26 @@ class _MapsPageState extends State<MapsPage> {
   final Map<String, Marker> _markers = {};
   final LatLng _center = const LatLng(0, 0); // Default center of the map
 
-  // Method to handle map creation and fetch markers
+  // Method to handle map creation and fetch markers from assets location.json
   Future<void> _onMapCreated(GoogleMapController controller) async {
     mapController = controller;
-    final googleOffices = await locations.getGoogleOffices();
+    // Load the location data from assets/location.json
+    final String response = await rootBundle.loadString('assets/location.json');
+    final data = json.decode(response);
 
     setState(() {
       _markers.clear();
-      for (final office in googleOffices.offices) {
+      // Loop through the clinics data and create markers
+      for (var clinic in data['clinics']) {
         final marker = Marker(
-          markerId: MarkerId(office.name),
-          position: LatLng(office.lat, office.lng),
+          markerId: MarkerId(clinic['name']),
+          position: LatLng(clinic['lat'], clinic['lng']),
           infoWindow: InfoWindow(
-            title: office.name,
-            snippet: office.address,
+            title: clinic['name'],
+            snippet: clinic['address'],
           ),
         );
-        _markers[office.name] = marker;
+        _markers[clinic['name']] = marker;
       }
     });
   }
@@ -39,7 +43,7 @@ class _MapsPageState extends State<MapsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Google Office Locations'),
+        title: const Text('Mental Health Clinics Locations'),
         elevation: 2,
         backgroundColor: Colors.green[700],
       ),
@@ -47,7 +51,7 @@ class _MapsPageState extends State<MapsPage> {
         onMapCreated: _onMapCreated,
         initialCameraPosition: const CameraPosition(
           target: LatLng(0, 0), // Center the map initially
-          zoom: 2,             // Default zoom level
+          zoom: 2,               // Default zoom level
         ),
         markers: _markers.values.toSet(),
       ),
